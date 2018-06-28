f9d34eae1b430a1686262c5ca5fae660a6d4b848
ariel-dono <herrerafariel.345@gmail.com>
2018-06-28 02:48:10 -0400

    Asyncronic response

--- ejemplo ZMQ/worker.js
+++ ejemplo ZMQ/worker.js
@@ -1,7 +1,4 @@
-// Hello World server
-// Binds REP socket to tcp://*:5555
-// Expects "Hello" from client, replies with "world"
-
+//Worker definition and ZMQ Connection
 var zmq = require('zmq');
 
 function dispatcher(port, action){
@@ -25,6 +22,7 @@ function dispatcher(port, action){
   });
 }
 
+//Actions
 function actionPrint(request, responder, port){
   console.log("Received request: [", request.toString(), "]");
   responder.send(`Atendido por el proceso: ${process.pid}, Puerto: ${port}, Received request: [${request.toString()}]`);
@@ -34,3 +32,5 @@ dispatcher(5555, actionPrint);
 dispatcher(5556, actionPrint);
 dispatcher(5557, actionPrint);
 dispatcher(5558, actionPrint);
+
+//Initialization
--- ejemplo polka/index.js
+++ ejemplo polka/index.js
@@ -3,28 +3,33 @@ var zmq = require('zmq');
 
 const app = polka();
 
-// socket to talk to server
+// ZMQ Connection
 console.log("Connecting to zmq server…");
 
 function getConnection(port){
-  var requester = zmq.socket('req');
+  let requester = zmq.socket('req');
   requester.connect(`tcp://localhost:${port}`);
+
+  requester.on("message", function(reply) {
+    console.log(reply.toString());
+  });
+
   return requester;
 }
 
+//Pool connections
 getter = getConnection(5555);
 updater = getConnection(5556);
 maker = getConnection(5557);
 deleter = getConnection(5558);
 
+// Actions
 function response(requester, res, message){
   requester.send(message);
-  requester.on("message", function(reply) {
-    console.log(reply.toString());
-    res.end(reply);
-  });
+  res.end(message);
 }
 
+//Rest Definition
 app.get('/', (req, res) => {
   response(getter, res, `Client ${process.pid}: Polka Rest API`);
 });
@@ -49,6 +54,7 @@ app.delete('/users/:id', (req, res) => {
   response(deleter, res, `Client ${process.pid}: Delete data of User ${req.params.id}`);
 });
 
+//Server initialization
 app.listen(4200).then(_ => {
     console.log(`> Running on localhost:4200`);
-})
+});
