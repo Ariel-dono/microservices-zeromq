2ab9563573a8dd7f637c6769d77e56a0bd992e18
ariel-dono <herrerafariel.345@gmail.com>
2018-09-15 21:20:39 -0400

    scope error low resources env

--- ejemplo polka/zmq_handler.js
+++ ejemplo polka/zmq_handler.js
@@ -5,13 +5,13 @@ console.log("Connecting to zmq server…");
 
 module.exports = {
     getConnection: function(port){
-    let requester = zmq.socket('req');
-    requester.connect(`tcp://localhost:${port}`);
+        let requester = zmq.socket('req');
+        requester.connect(`tcp://localhost:${port}`);
 
-    requester.on("message", function(reply) {
-        console.log(reply.toString());
-    });
+        requester.on("message", function(reply) {
+            console.log(reply.toString());
+        });
 
-    return requester;
+        return requester;
     }
 }
